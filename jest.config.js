522ff1cfffe05f19184c237be513da5a5f3bc79f
brpro.wrlover@gmail.com <brpro.wrlover@gmail.com>
2024-06-19 23:16:03 +0100

    auth pages test, starting test for all projects page

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ export default {
     "^@/mocks/(.*)$": "<rootDir>/__mocks__/$1",
     "^@/services/(.*)$": "<rootDir>/src/redux/services/$1",
     "^@/slices/(.*)$": "<rootDir>/src/redux/slices/$1",
+    "^@/ui/(.*)$": "<rootDir>/src/components/ui/$1",
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
